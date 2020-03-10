946c3939b06a1e418939408f4d269330e365fb8c
bacs93 <36246209+bacs93@users.noreply.github.com>
2020-03-09 21:53:48 -0300

    Add files via upload

---
+++ Lista Exercicios 1/Exercicio 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Total de votos nulos: ");
+            int nulos = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Total de votos brancos: ");
+            int brancos = int.Parse(Console.ReadLine());
+
+            Console.Write("Total de votos válidos: ");
+            int validos = int.Parse(Console.ReadLine());
+
+            int total = nulos + brancos + validos;
+
+            Console.Write("\nTotal de votos: " + total);
+
+            Console.Write("\nVotos nulos: " + (nulos*100/total) +"%");
+
+            Console.Write("\nVotos brancos: " + (brancos * 100 / total) + "%");
+
+            Console.Write("\nVotos validos: " + (validos * 100 / total) + "%");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista Exercicios 1/Lista Exercicios 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Exercicios_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            Console.Write("Mes: ");
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Dia: ");
+            int dia = int.Parse(Console.ReadLine());
+
+            int idade_total = ((2020-ano)*365) - (mes*30) - (dia);
+
+            Console.Write("Idade em dias: " + idade_total);
+
+            Console.ReadKey();
+        }
+    }
+}
