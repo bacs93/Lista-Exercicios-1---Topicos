183a3a9677509bc6727561cf1dd89882a76f8105
bacs93 <36246209+bacs93@users.noreply.github.com>
2020-04-20 20:04:56 -0300

    Add files via upload

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o custo de fábrica: ");
+            decimal custo_fabrica = decimal.Parse(Console.ReadLine());
+
+            decimal perc_distribuideor = 0.28m;
+
+            decimal perc_impostos = 0.45m;
+
+            decimal distribuidor = custo_fabrica * perc_distribuideor;
+
+            decimal impostos = custo_fabrica * perc_impostos;
+
+            decimal custo_final = custo_fabrica + distribuidor + impostos;
+
+            Console.Write("\nPorcentagem do distribuidor: " + distribuidor);
+
+            Console.Write("\nValor dos Impostos: " + impostos);
+
+            Console.Write("\nCusto final: " + (custo_fabrica + distribuidor + impostos));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Salário fixo mensal: ");
+            decimal s_fixo = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Valor da comissão fixa: ");
+            decimal c_fixa = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de carros vendidos: ");
+            int carros_vedidos = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor total das vendas: ");
+            decimal total_vendas = decimal.Parse(Console.ReadLine());
+
+            decimal perc_comissao = 0.05m;
+
+            decimal salario = total_vendas * perc_comissao;
+
+            Console.Write("\nSalário final: " + (salario + s_fixo + carros_vedidos * c_fixa));
+
+            Console.ReadKey();
+        }
+    }
+}
